eca051a75a839f8a2980f665da60ea8b1a600fc4
sboy99 <shadowboy191@gmail.com>
2022-08-18 01:27:21 +0000

    Component & Pages created

---
+++ src/components/buttons/AmountButtons.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AmountButtons = () => {
+  return <div>AmountButtons</div>;
+};
+
+export default AmountButtons;
---
+++ src/components/products/FeaturedProduct.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeaturedProduct = () => {
+  return <div>FeaturedProduct</div>;
+};
+
+export default FeaturedProduct;
