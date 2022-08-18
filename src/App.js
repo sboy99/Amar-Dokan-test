0edc73891db3de5e6171312e72fc9b838212df94
sboy99 <shadowboy191@gmail.com>
2022-08-18 23:12:41 +0000

    Routing updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout, Protected } from "./components";
+import {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Cart,
+  CheckOut,
+  Error,
+} from "./pages";
 
 const App = () => {
-  return <div className="bg-indigo-600">App</div>;
+  return (
+    <section>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
+          <Route path="cart" element={<Cart />} />
+          <Route
+            path="checkout"
+            element={
+              <Protected>
+                <CheckOut />
+              </Protected>
+            }
+          />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </section>
+  );
 };
 
 export default App;
