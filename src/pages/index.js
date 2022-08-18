29d9b635cb7a14b5dc423fdd0ea28d45904cce77
sboy99 <shadowboy191@gmail.com>
2022-08-18 01:21:16 +0000

    Component & Pages created

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import AboutPage from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import CartPage from "./CartPage";
+import CheckOut from "./CheckOut";
+import ErrorPage from "./ErrorPage";
+import ProductPage from "./ProductPage";
+import SingleProductPage from "./SingleProductPage";
+
+export {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckOut,
+  ErrorPage,
+  ProductPage,
+  SingleProductPage,
+};
