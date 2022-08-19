c3e49a7fb6f9379818d796afbdceac8884cff809
sboy99 <shadowboy191@gmail.com>
2022-08-19 00:19:21 +0000

    Navbar started

--- src/components/sub-pages/Navbar.jsx
+++ src/components/sub-pages/Navbar.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { MenuIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="relative h-16 text-gray-700 dark:text-gray-100">
+      <div className="container mx-auto flex h-full items-center justify-between px-4">
+        {/* links */}
+        <div className="flex items-center gap-3 font-bold">
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/products"}>Products</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+        </div>
+        {/* buttons */}
+        <div className="flex items-center">
+          <NavLink
+            to={"/signin"}
+            className="flex items-center justify-center rounded-md bg-secondary-dark-bg px-4 py-1 font-bold capitalize tracking-wide text-gray-100 dark:bg-gray-100 dark:text-gray-700"
+          >
+            Sign In
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
