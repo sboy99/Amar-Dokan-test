a981c6827da48594a45ad9e937d8a54c74d86223
sboy99 <shadowboy191@gmail.com>
2022-08-19 00:21:02 +0000

    Navbar Styled

--- src/App.js
+++ src/App.js
@@ -13,26 +13,28 @@ import {
 
 const App = () => {
   return (
-    <section>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:id" element={<SingleProduct />} />
-          <Route path="cart" element={<Cart />} />
-          <Route
-            path="checkout"
-            element={
-              <Protected>
-                <CheckOut />
-              </Protected>
-            }
-          />
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
-    </section>
+    <div className="">
+      <section className=" min-h-screen w-full bg-primary-light-bg text-gray-700 dark:bg-primary-dark-bg dark:text-gray-100">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/:id" element={<SingleProduct />} />
+            <Route path="cart" element={<Cart />} />
+            <Route
+              path="checkout"
+              element={
+                <Protected>
+                  <CheckOut />
+                </Protected>
+              }
+            />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </section>
+    </div>
   );
 };
 
