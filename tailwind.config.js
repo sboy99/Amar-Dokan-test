4debbd43a69e32d4015a2f6d114bbd4bdc77270d
sboy99 <shadowboy191@gmail.com>
2022-08-21 00:53:23 +0000

    New Dependency installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xss: "10px",
+        14: "14px",
+      },
+      backgroundColor: {
+        "primary-light-bg": "#FAFBFB",
+        "primary-dark-bg": "#20232A",
+        "secondary-light-bg": "",
+        "secondary-dark-bg": "#33373E",
+        "transparent-light": "rgba(255,255,255,0.5)",
+        "transparent-dark": "rgba(0,0,0,0.5)",
+      },
+      width: {
+        400: "400px",
+        760: "760px",
+        780: "780px",
+        800: "800px",
+        1000: "1000px",
+        1200: "1200px",
+        1280: "1280px",
+        1400: "1400px",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
