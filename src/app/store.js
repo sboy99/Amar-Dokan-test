507a4337409aae0bfd4863f4cdbcf1fd6697b165
sboy99 <shadowboy191@gmail.com>
2022-08-23 01:15:11 +0000

    LayoutSlice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { LayoutReducer } from "../features";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    Layout: LayoutReducer,
+  },
 });
+
+export const Layout = (store) => store.Layout;
